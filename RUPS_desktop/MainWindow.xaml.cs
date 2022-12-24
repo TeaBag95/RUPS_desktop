39f9c4079aef3182e944300d228e826fdd7e4980
Mitja Polner <mitja@maked-energea.si>
2022-12-24 23:07:01 +0100

    Dodana navigacija

--- RUPS_desktop/MainWindow.xaml.cs
+++ RUPS_desktop/MainWindow.xaml.cs
@@ -20,9 +20,55 @@ namespace RUPS_desktop
     /// </summary>
     public partial class MainWindow : Window
     {
+        HomePage homePage = new HomePage();
+        ListPage listPage = new ListPage();
+        MapPage mapPage = new MapPage();
+
+        Color selected = (Color)ColorConverter.ConvertFromString("#002171");
+        Color unselected = (Color)ColorConverter.ConvertFromString("#0d47a1");
+        Color selectedText = (Color)ColorConverter.ConvertFromString("#fbc02d");
+
         public MainWindow()
         {
             InitializeComponent();
+            MainFrame.Content = homePage;
+            HomePage_btn.Background = new SolidColorBrush(selected);
+        }
+
+        private void HomePage_btn_Click(object sender, RoutedEventArgs e)
+        {
+            MainFrame.Content = homePage;
+            HomePage_btn.Background = new SolidColorBrush(selected);
+            ListPage_btn.Background = new SolidColorBrush(unselected);
+            MapPage_btn.Background = new SolidColorBrush(unselected);
+
+            HomePage_btn.Foreground = new SolidColorBrush(selectedText);
+            ListPage_btn.Foreground = Brushes.White;
+            MapPage_btn.Foreground = Brushes.White;
+        }
+
+        private void ListPage_btn_Click(object sender, RoutedEventArgs e)
+        {
+            MainFrame.Content = listPage;
+            HomePage_btn.Background = new SolidColorBrush(unselected);
+            ListPage_btn.Background = new SolidColorBrush(selected);
+            MapPage_btn.Background = new SolidColorBrush(unselected);
+
+            HomePage_btn.Foreground = Brushes.White;
+            ListPage_btn.Foreground = new SolidColorBrush(selectedText);
+            MapPage_btn.Foreground = Brushes.White;
+        }
+
+        private void MapPage_btn_Click(object sender, RoutedEventArgs e)
+        {
+            MainFrame.Content = mapPage;
+            HomePage_btn.Background = new SolidColorBrush(unselected);
+            ListPage_btn.Background = new SolidColorBrush(unselected);
+            MapPage_btn.Background = new SolidColorBrush(selected);
+
+            HomePage_btn.Foreground = Brushes.White;
+            ListPage_btn.Foreground = Brushes.White;
+            MapPage_btn.Foreground = new SolidColorBrush(selectedText);
         }
     }
 }

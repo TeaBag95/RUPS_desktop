c9023d5b6a2574c16a956dc1234dd748dc2df4f5
Mitja Polner <mitja@maked-energea.si>
2022-12-27 00:53:18 +0100

    Login and registration UI

---
+++ RUPS_desktop/LoginPage.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace RUPS_desktop
+{
+    /// <summary>
+    /// Interaction logic for LoginPage.xaml
+    /// </summary>
+    public partial class LoginPage : Page
+    {
+        public LoginPage()
+        {
+            InitializeComponent();
+        }
+
+        public void Login_btn_Click(object sender, RoutedEventArgs e)
+        {
+            MainWindow mainWindow = new MainWindow();
+            mainWindow.Show();
+            LoginWindow parentWindow = (LoginWindow)Window.GetWindow(this);
+            parentWindow.Close();
+        }
+    }
+}

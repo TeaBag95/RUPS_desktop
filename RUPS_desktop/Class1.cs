05d50f6feff81aee8a4dafd9f086d02a2e7dc735
benjoboy <serec.benjamin@gmail.com>
2022-12-26 22:21:24 +0100

    find closest station feature

---
+++ RUPS_desktop/Class1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.Json.Serialization;
+using Newtonsoft.Json;
+namespace RUPS_desktop
+{
+
+    public class Station
+    {
+        public string type { get; set; }
+        public string id { get; set; }
+        public Properties properties { get; set; }
+        public Geometry geometry { get; set; }
+    }
+    // Root myDeserializedClass = JsonConvert.DeserializeObject<Root>(myJsonResponse);
+    public class Geometry
+    {
+        public string type { get; set; }
+        public List<double> coordinates { get; set; }
+    }
+
+    public class Properties
+    {
+        [JsonProperty("marker-color")]
+        public string markercolor { get; set; }
+
+        [JsonProperty("marker-size")]
+        public string markersize { get; set; }
+
+        [JsonProperty("marker-symbol")]
+        public string markersymbol { get; set; }
+
+        [JsonProperty("marker-title")]
+        public string markertitle { get; set; }
+        public string number { get; set; }
+        public string name { get; set; }
+        public string address { get; set; }
+        public string busroute { get; set; }
+        public string URL { get; set; }
+    }
+
+  
+
+
+}
--- RUPS_desktop/MapPage.xaml.cs
+++ RUPS_desktop/MapPage.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.Maps.MapControl.WPF;
+using System;
 using System.Collections.Generic;
 using System.Text;
 using System.Windows;
@@ -10,6 +11,11 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Diagnostics;
+using System.Net.Http;
+using System.IO;
+using System.Net;
+using Newtonsoft.Json;
 
 namespace RUPS_desktop
 {
@@ -18,9 +24,48 @@ namespace RUPS_desktop
     /// </summary>
     public partial class MapPage : Page
     {
+
         public MapPage()
         {
             InitializeComponent();
+            myMap.Mode = new AerialMode(true);
         }
+
+        private void myMap_Loaded(object sender, RoutedEventArgs e)
+        {
+            myMap.Center = new Location(46.14, 15);
+            myMap.ZoomLevel = 8;
+
+        }
+
+        private void FindLocation_btn_Click(object sender, RoutedEventArgs e)
+        {
+            Location curr = myMap.Center;
+
+            //print
+            Trace.WriteLine("lat: " + curr.Latitude + " long: " + curr.Longitude);
+
+            string url = "http:\\\\localhost:3002/api/station/nearest?lat=" + curr.Latitude.ToString().Replace(',', '.') + "&lon=" + curr.Longitude.ToString().Replace(',', '.');
+            Trace.WriteLine(url);
+            string html = string.Empty;
+            
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url);
+                
+            using (HttpWebResponse response = (HttpWebResponse)request.GetResponse())
+            using (Stream stream = response.GetResponseStream())
+            using (StreamReader reader = new StreamReader(stream))
+            {
+                html = reader.ReadToEnd();
+            }
+
+            Station myDeserializedClass = JsonConvert.DeserializeObject<Station>(html);
+
+            Trace.WriteLine(myDeserializedClass.geometry.coordinates[0] + " " + myDeserializedClass.geometry.coordinates[1]);
+            myMap.Center= new Location(myDeserializedClass.geometry.coordinates[1], myDeserializedClass.geometry.coordinates[0]);
+            myMap.ZoomLevel = 18;
+
+
+        }
+
     }
 }

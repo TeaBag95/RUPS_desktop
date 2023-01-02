75c450c8d3525b2db111fabdf26c0a77e517b376
benjoboy <serec.benjamin@gmail.com>
2023-01-02 20:06:06 +0100

    seznam dodal search

--- RUPS_desktop/ListPage.xaml.cs
+++ RUPS_desktop/ListPage.xaml.cs
@@ -12,6 +12,8 @@ using System.Windows.Navigation;
 using System.Windows.Shapes;
 using System.Net.Http;
 using Newtonsoft.Json;
+using System.Diagnostics;
+
 namespace RUPS_desktop
 {
     /// <summary>
@@ -28,10 +30,10 @@ namespace RUPS_desktop
         private async void SearchButton_Click(object sender, RoutedEventArgs e)
         {
             string searchTerm = SearchTextBox.Text;
-
+            Trace.WriteLine("here");
             using (var client = new HttpClient())
             {
-                client.BaseAddress = new Uri("http://localhost:3002/api/station/");
+                client.BaseAddress = new Uri("http://localhost:3002/api/station/" + searchTerm);
                 var request = new HttpRequestMessage(HttpMethod.Post, "");
                 var response = await client.SendAsync(request);
                 var responseString = await response.Content.ReadAsStringAsync();
@@ -40,20 +42,25 @@ namespace RUPS_desktop
 
             using (var client = new HttpClient())
             {
-                client.BaseAddress = new Uri("http://localhost:3002/api/station/");
+                client.BaseAddress = new Uri("http://localhost:3002/api/station/" + searchTerm);
 
                 var response = await client.GetAsync($"?prefix={searchTerm}");
                 var responseString = await response.Content.ReadAsStringAsync();
 
-                var stations = JsonConvert.DeserializeObject<List<Stations>>(responseString);
+                var stations = JsonConvert.DeserializeObject<List<StationDetails>>(responseString);
 
                 ResultsListView.ItemsSource = stations;
             }
         }
     }
 
-    public class Stations
+    public class StationDetails
     {
-        public string Name { get; set; }
+        public int id { get; set; }
+        public string name { get; set; }
+        public string address { get; set; }
+        public string url { get; set; }
+        public double latitude { get; set; }
+        public double longitude { get; set; }
     }
 }
